f74fab8cc3d43fa1a58f0a81a1afb063bb76b70a
Sacha Chua <sacha@sachachua.com>
2021-04-16 00:21:11 -0400

    Initial

---
+++ ox-11ty.el
@@ -0,0 +1,80 @@
+;;; ox-11ty.el --- Eleventy export for Emacs Org Mode  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Sacha Chua
+
+;; Author: Sacha Chua <sacha@sachachua.com>
+;; Version: 2.17.0
+;; Package-Requires: ((emacs "27"))
+;; Keywords: org, eleventy, 11ty
+;; Homepage: https://github.com/sachac/ox-11ty.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A very rough starting point for exporting to 11ty from Org Mode.
+;;
+
+;;; Code:
+
+(require 'ox-html)
+
+(defun org-11ty-template (contents info)
+  (let* ((date (org-export-data (plist-get info :date) info))
+         (title (org-export-data (plist-get info :title) info))
+         (permalink (org-export-data (plist-get info :permalink) info))
+         (categories (org-export-data (plist-get info :categories) info))
+         (collections (org-export-data (plist-get info :collections) info))
+         (front-matter (json-encode
+                        (list :permalink permalink
+                              :date date
+                              :title title
+                              :categories (split-string categories)
+                              :tags (split-string collections)))))
+    (format
+     "module.exports = class {
+  data() {
+    return %s;
+  }
+  render() {
+    return %s;
+  }
+}"
+     front-matter
+     (json-encode-string contents))))
+
+(defun org-11ty-export-as-11ty (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer as 11ty file."
+  (interactive)
+  (org-export-to-buffer '11ty "*org 11ty export*" async subtreep visible-only body-only ext-plist))
+
+(defun org-11ty-export-to-11ty (&optional async subtreep visible-only body-only ext-plist)
+  (interactive)
+  (let ((file (org-export-output-file-name ".11ty.js" subtreep)))
+    (org-export-to-file '11ty file
+      async subtreep visible-only body-only ext-plist)))
+
+(org-export-define-derived-backend '11ty 'html
+  :menu-entry
+  '(?1 "Export to 11ty JS"
+       ((?b "As buffer" org-11ty-export-as-11ty) 
+        (?1 "To file" org-11ty-export-to-11ty)))
+  :translate-alist
+  '((template . org-11ty-template))
+  :options-alist
+  '((:permalink "PERMALINK" nil nil)
+    (:categories "CATEGORIES" nil nil)
+    (:collections "COLLECTIONS" nil 'parse)))
+
+;;; ox-11ty.el ends here
